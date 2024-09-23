2a2cc5363255bdb9f0a0338413d48237c3891b9c
pleufang-ext <paul.leufang-ext@be-ys.com>
2024-09-24 00:44:21 +0100

    Feat : Add tasks in the todo list

--- src/test/java/example/SeleniumTest.java
+++ src/test/java/example/SeleniumTest.java
@@ -1,11 +1,18 @@
 package example;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,6 +38,42 @@ public class SeleniumTest {
 
         //then
         assertEquals("TodoMVC: React", driver.getTitle());
+    }
+
+    @Test
+    public void shouldAddATaskInATheTodoList() {
+        //given
+        driver.get("https://todomvc.com/examples/react/dist/");
+        assertEquals("TodoMVC: React", driver.getTitle());
+
+        //When
+        WebElement input = driver.findElement(By.cssSelector(".new-todo"));
+        input.sendKeys("Go to the supermarket");
+        input.sendKeys(Keys.ENTER);
+        takeScreenshot("1");
+        input.sendKeys("Read a book");
+        input.sendKeys(Keys.ENTER);
+        takeScreenshot("2");
+
+        //Then
+        List<String> tasks = Arrays.asList("Go to the supermarket","Read a book");
+
+        List<WebElement> labels = driver.findElements(By.xpath("//ul//li//label"));
+
+        assertEquals(tasks.get(0), labels.get(0).getText());
+        assertEquals(tasks.get(1), labels.get(1).getText());
+        takeScreenshot("3");
+    }
+
+    public void takeScreenshot(String fileName) {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("target/screenshots/" + fileName + ".png");
+        try {
+            FileUtils.copyFile(srcFile, destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
